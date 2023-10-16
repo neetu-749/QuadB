8e83e417c40bfff94664d84e5a58badd2b808eee
A-K-2001 <kumaranilstps017@gmail.com>
2023-10-16 15:55:25 +0530

    check test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Card from './components/Card';
 import Summary from './pages/Summary';
+// 
+
+// try new laptop
 
 function App() {
   return (
